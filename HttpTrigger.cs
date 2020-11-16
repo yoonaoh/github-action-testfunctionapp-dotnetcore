50f5569a8d2be52905f1eff330de109960c25886
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-11-16 10:23:57 -0800

    Initial Commit

---
+++ HttpTrigger.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using IronSnappy;
+
+namespace github_action_testfunctionapp_dotnetcore
+{
+    public static class HttpTrigger
+    {
+        [FunctionName("HttpTrigger")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            byte[] input = File.ReadAllBytes("host.json");
+            Snappy.Encode(input);
+
+            log.LogInformation("Sucessfully loaded IronSnappy");
+
+            string responseMessage = "Hello Functions World!";
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
